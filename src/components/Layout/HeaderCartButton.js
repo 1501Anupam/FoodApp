17cc670e8efd8df91a8caa8dc837a1044c42354f
Anupam Choudhary <iamanupam1501@gmail.com>
2022-11-04 00:28:40 +0530

    Added provider

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,20 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [isBtnHighlighted, setIsBtnHighlighted] = useState(true);
   const cartCtx = useContext(CartContext);
+
+  const btnClasses = `${classes.button} ${classes.bump}`;
 
   const numberOfItems = cartCtx.items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
